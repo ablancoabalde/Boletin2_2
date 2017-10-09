9d1ccd03e8ddbd02d6102965487d6d61f6cc7036
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-09 08:48:11 +0200

    Documento ejecutable

---
+++ src/boletin2_2/Boletin2_2.java
@@ -0,0 +1,16 @@
+
+package boletin2_2;
+
+public class Boletin2_2 {
+
+    public static void main(String[] args) {
+
+        float lado=3.4F;
+
+       
+        System.out.println("El area de un cuadrado es: " +(Math.pow(lado, 2)));
+        
+        
+    }
+    
+}
